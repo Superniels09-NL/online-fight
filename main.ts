c34f704ae248f34f97aa48877599095fdc3b6724
Superniels09-NL <67866688+Superniels09-NL@users.noreply.github.com>
2021-02-02 13:57:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,9 @@ enum RadioMessage {
     A = 18289,
     B = 9031,
     fight = 45636,
-    DED = 18817
+    DED = 18817,
+    ready = 31336,
+    yes = 16427
 }
 radio.onReceivedMessage(RadioMessage.A, function () {
     Player2_X += -1
@@ -26,10 +28,22 @@ radio.onReceivedMessage(RadioMessage.fight, function () {
     Display()
 })
 input.onButtonPressed(Button.A, function () {
-    if (!(Player_X == 0)) {
-        Player_X += -1
-        Display()
-        radio.sendMessage(RadioMessage.A)
+    if (ready == 1 && _2_ready == 1) {
+        if (!(Player_X == 0)) {
+            Player_X += -1
+            Display()
+            radio.sendMessage(RadioMessage.A)
+        }
+    }
+})
+radio.onReceivedMessage(RadioMessage.ready, function () {
+    if (ready == 1) {
+        _2_ready = 1
+        Player_X = 4
+        radio.sendMessage(RadioMessage.yes)
+    } else {
+        _2_ready = 1
+        admin = 2
     }
 })
 function Display () {
@@ -46,54 +60,86 @@ function Display () {
     }
 }
 input.onButtonPressed(Button.AB, function () {
-    rocket_in_screen = true
-    radio.sendMessage(RadioMessage.fight)
-    rocket_X = Player_X
-    rocket_Y = 4
-    for (let index = 0; index < 4; index++) {
-        rocket_Y += -1
+    if (ready == 1 && _2_ready == 1) {
+        rocket_in_screen = true
+        radio.sendMessage(RadioMessage.fight)
+        rocket_X = Player_X
+        rocket_Y = 4
+        for (let index = 0; index < 4; index++) {
+            rocket_Y += -1
+            Display()
+            basic.pause(100)
+        }
+        rocket_in_screen = false
         Display()
-        basic.pause(100)
     }
-    rocket_in_screen = false
-    Display()
 })
 radio.onReceivedMessage(RadioMessage.DED, function () {
     Game_end = true
     basic.showIcon(IconNames.Yes)
 })
 input.onButtonPressed(Button.B, function () {
-    if (!(Player_X == 4)) {
-        Player_X += 1
-        Display()
-        radio.sendMessage(RadioMessage.B)
+    if (ready == 1 && _2_ready == 1) {
+        if (!(Player_X == 4)) {
+            Player_X += 1
+            Display()
+            radio.sendMessage(RadioMessage.B)
+        }
     }
 })
 radio.onReceivedMessage(RadioMessage.B, function () {
     Player2_X += 1
     Display()
 })
-radio.onReceivedMessage(RadioMessage.Connect, function () {
-    Player_X = 2
-    Player2_X = 2
+radio.onReceivedMessage(RadioMessage.yes, function () {
+    Player2_X = 4
 })
 let rocket_X = 0
 let rocket_in_screen = false
+let admin = 0
+let _2_ready = 0
+let ready = 0
+let Player_X = 0
 let rocket_2_X = 0
 let rocket_2_in_screen = false
+let Player2_X = 0
 let rocket_2_Y = 0
 let rocket_Y = 0
 let Game_end = false
-let Player2_X = 0
-let Player_X = 0
-Player_X = 2
-Player2_X = 2
 Game_end = false
 let HP = 5
 radio.sendMessage(RadioMessage.Connect)
 rocket_Y = 100
 rocket_2_Y = 100
-Display()
+basic.forever(function () {
+    if (_2_ready == 1 && !(ready == 1)) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            # # # # #
+            `)
+    }
+    if (ready == 1 && !(_2_ready == 1)) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+    if (ready == 0 && input.buttonIsPressed(Button.AB)) {
+        radio.sendMessage(RadioMessage.ready)
+        ready = 1
+        if (admin == 0) {
+            admin = 1
+        }
+    }
+})
 basic.forever(function () {
     if (HP == 0 && Game_end == false) {
         radio.sendMessage(RadioMessage.DED)
